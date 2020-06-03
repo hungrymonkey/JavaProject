88de4221f8e597c8f0046eb7e2ac917301b7edc5
hungrymonkey <boringmonkey@gmail.com>
2020-06-03 14:32:44 -0700

    Added map title

--- flutter_web/lib/my_map1.dart
+++ flutter_web/lib/my_map1.dart
@@ -102,36 +102,28 @@ class _MyMapPageState1 extends State<MyMapPage1> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Center(
-          child: Wrap(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            children: <Widget>[
-              ConstrainedBox(
-                constraints: BoxConstraints(maxWidth: 300, maxHeight: 300),
-                child: SimpleScatterPlotChart(
-                  _createSampleData(),
-                  // Disable animations for image tests.
-                  animate: false,
-                ),
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.all(30), 
+                child: Text(widget.title, style: Theme.of(context).textTheme.headline2),
               ),
-              ConstrainedBox(
-                constraints: BoxConstraints(maxWidth: 300, maxHeight: 300),
-                child: locationMap,
+              Wrap(
+                children: <Widget>[
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 300, maxHeight: 300),
+                    child: SimpleScatterPlotChart(
+                      _createSampleData(),
+                      // Disable animations for image tests.
+                      animate: false,
+                    ),
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 300, maxHeight: 300),
+                    child: locationMap,
+                  ),
+                ],
               ),
             ],
-        
           ),
         ),
       ),
